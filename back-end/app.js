44b461317454cdd85e83c4879fde5b103a8bd143
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-01-13 00:15:51 +0700

    'test-api-v1'

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,30 @@
-console.log('test nodemon');
+require('dotenv').config();
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+const PORT = process.env.PORT;
+const api = process.env.API_URL;
+
+/**middleware */
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
-   res.json({msg : 'test router'});
-})
-app.listen(6969, () => { console.log('server run di port 6969')});
+   res.json({ msg : 'test router'});
+});
+
+app.get(`${api}/products` , (req, res) => {
+   const dataProduct = {
+      id :1,
+      product : 'kertas',
+      price : '6000'
+   }
+   res.json({product : dataProduct})
+});
+
+app.post(`${api}/products` ,(req, res) => {
+   const newProduct = req.body;
+   console.log(newProduct);
+    res.json(newProduct);
+});
+
+
+app.listen(PORT, () => { console.log(`server run di port ${PORT}`)});
